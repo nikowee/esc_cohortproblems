6344090a2081c5dd49acfe11e362c14e537d738a
nicholas <118549527+nikowee@users.noreply.github.com>
2026-06-07 13:45:33 +0800

    CE2 completed

--- ce2/ce2_q1/routes/dept.ts
+++ ce2/ce2_q1/routes/dept.ts
@@ -6,7 +6,11 @@ const router: Router = express.Router();
 
 
 router.get('/add/:code', async function(_req: Request, res: Response, _next: NextFunction) {
-    res.send(`TODO`); // TODO: Fixme
+    const code = _req.params.code;
+    const dept = new deptmodel.Dept(code);
+    await deptmodel.insertMany([dept]);
+
+    res.json({code}); // TODO: Fixme
 });
 
 
@@ -14,13 +18,18 @@ router.get('/add/:code', async function(_req: Request, res: Response, _next: Nex
 /* GET dept listing. */
 
 router.get('/all/', async function(_req: Request, res: Response, _next: NextFunction) {
-    res.send(`TODO`); // TODO: Fixme
+    const depts = await deptmodel.all()
+    res.send(depts); // TODO: Fixme
 });
 
 
 router.get('/all/withstaff/', async function(_req: Request, res: Response, _next: NextFunction) {
-    res.send(`TODO`); // TODO: Fixme
+    const staffs = await staffmodel.all();
+    const groupedStaffs = Object.groupBy(staffs, (staff: any) => staff.dept);
+    const formattedOutput = Object.entries(groupedStaffs).map(([code, staffs]) => ({code, staffs}));
+    res.send(formattedOutput); // TODO: Fixme
 })
+
 
 
 export default router;
--- ce2/ce2_q1/routes/staff.ts
+++ ce2/ce2_q1/routes/staff.ts
@@ -7,13 +7,20 @@ const router: Router = express.Router();
 
 /* insert a staff, should have used POST instead of GET */
 router.get('/add/:id/:name/:code', async function(_req: Request, res: Response, _next: NextFunction) {
-    res.send(`TODO`); // TODO: Fixme
+    const id = _req.params.id;
+    const name = _req.params.name;
+    const code = _req.params.code;
+
+    const staff = new staffmodel.Staff(id, name, code);
+    await staffmodel.insertMany([staff]);
+    res.send(staff); // TODO: Fixme
 });
 
 /* GET staff listing. */
 
 router.get('/all/', async function(_req: Request, res: Response, _next: NextFunction) {
-    res.send(`TODO`); // TODO: Fixme
+    const staffs = await staffmodel.all();
+    res.send(staffs); // TODO: Fixme
 });
 
 
